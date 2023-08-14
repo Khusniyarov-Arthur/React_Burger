826f890f8d95ebdb4c8e99f844301a8307956f89
Khusniyarov-Arthur <Khusniyarovaa@yandex.ru>
2023-08-14 22:09:35 +0300

    Created a project, added a head and a container.

---
+++ src/components/Container/Container.jsx
@@ -0,0 +1,19 @@
+import style from "./container.module.css";
+import classNames from "classnames";
+import PropTypes from "prop-types";
+
+export const Container = ({ children, className }) => {
+  return (
+    <div className={classNames(style.container, className)}>{children}</div>
+  );
+};
+
+Container.propTypes = {
+  className: PropTypes.string,
+  children: PropTypes.oneOfType([
+    PropTypes.string,
+    PropTypes.object,
+    PropTypes.array,
+    PropTypes.number,
+  ]),
+};
