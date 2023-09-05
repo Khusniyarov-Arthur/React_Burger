a5f50e556b8177447785cfa2cfbd63691d7c3937
Khusniyarov-Arthur <Khusniyarovaa@yandex.ru>
2023-09-05 21:47:37 +0300

    change url container in navigate

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import style from "./header.module.css";
+import style from "./Header.module.css";
 import logo from "../../assets/img/logo.svg";
 import { Container } from "../Container/Container";
 
