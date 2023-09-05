9e2a138b53fe6cdf1d20dbe26e18e03f97bcfbe9
Khusniyarov-Arthur <Khusniyarovaa@yandex.ru>
2023-09-05 21:34:32 +0300

    change app

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Catalog } from "./components/Catalog/Catalog";
 import { Navigation } from "./components/Navigation/Navigation";
-import { Container } from "./components/container/container";
 import { Header } from "./components/header/header";
 
 export const App = () => {
@@ -9,7 +8,6 @@ export const App = () => {
       <Header />
       <main>
         <Navigation>
-          <Container className="navigation__container"></Container>
         </Navigation>
         <Catalog />
       </main>
